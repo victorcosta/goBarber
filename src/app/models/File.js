f3684dd423a9d232ca1250da01f6645b1f95aa8d
Victor Costa <victorcostadev@gmail.com>
2019-08-25 10:25:54 -0300

    adding provider list and file model

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,11 +1,17 @@
-import { Model, Sequelize } from 'sequelize';
+import Sequelize, { Model } from 'sequelize';
 
 class File extends Model {
   static init(sequelize) {
     super.init(
       {
+        path: Sequelize.STRING,
         name: Sequelize.STRING,
-        path: Sequelize.STRING
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          }
+        }
       },
       {
         sequelize
