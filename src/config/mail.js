e66a7e491719347e4055797bfaa424392c4256e4
Victor Costa <victorcostadev@gmail.com>
2019-08-26 01:36:48 -0300

    config nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b127e9e93e136b',
+    pass: 'f37fc776a9d688'
+  },
+  default: {
+    from: 'Equipe GoBarber <no-reply@gobarber.com>'
+  }
+};
